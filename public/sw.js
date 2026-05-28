d3c0b52625fa99d2226b86e97609d6eb64f716bc
levaqueangel <levaqueangel@gmail.com>
2026-05-28 12:20:50 +0200

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,35 +1,15 @@
+// Service Worker APXFITNESS
 const CACHE_NAME = 'apxfitness-v1';
-const STATIC_ASSETS = [
-  '/',
-  '/bilan',
-  '/mentions-legales',
-];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
-  );
+self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
 });
 
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const clone = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+self.addEventListener('fetch', (event) => {
+  // Laisser passer toutes les requêtes normalement
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
