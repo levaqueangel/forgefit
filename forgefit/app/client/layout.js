08597d60d7a72c341c3c9d21a719d5976c1b157a
levaqueangel <levaqueangel@gmail.com>
2026-05-28 23:46:50 +0200

    feat: add metadata to client page (noindex)

---
+++ forgefit/app/client/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Mon espace client | APXFITNESS",
+  description: "Accède à ton programme personnalisé et échange avec ton coach.",
+  robots: "noindex, nofollow",
+};
+
+export default function ClientLayout({ children }) {
+  return children;
+}
