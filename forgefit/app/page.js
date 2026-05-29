d77729c6599568763f26b690f4febe1534657083
levaqueangel <levaqueangel@gmail.com>
2026-05-29 17:15:13 +0200

    fix: force-dynamic to prevent static prerender for page

--- forgefit/app/page.js
+++ forgefit/app/page.js
@@ -1,4 +1,6 @@
 "use client";
+export const dynamic = "force-dynamic";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useLang } from "./useLang";
 import { LangSelector } from "./LangSelector";
