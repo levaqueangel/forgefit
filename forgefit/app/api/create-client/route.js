9365143ebf873203b12c4ed16c3dd2ca243c83a8
levaqueangel <levaqueangel@gmail.com>
2026-06-02 21:33:30 +0200

    fix(12): add rate limiting (10/min) to create-client/route.js

--- forgefit/app/api/create-client/route.js
+++ forgefit/app/api/create-client/route.js
@@ -1,5 +1,6 @@
 import { Resend } from "resend";
 import { getAdminAuth, getAdminDb } from "../firebase-admin";
+import { checkRateLimit } from "../rateLimit";
 export const dynamic = "force-dynamic";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
