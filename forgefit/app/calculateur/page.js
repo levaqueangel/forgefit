84a0dba124c76d4cdaf5c68ed118920792e6fc76
levaqueangel <levaqueangel@gmail.com>
2026-06-04 00:24:55 +0200

    feat: add BMI zone label to calculator results + useMemo import

--- forgefit/app/calculateur/page.js
+++ forgefit/app/calculateur/page.js
@@ -1,6 +1,6 @@
 "use client";
 export const dynamic = "force-dynamic";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { useRouter } from "next/navigation";
 import { useLang } from "../useLang";
 import { LangSelector } from "../LangSelector";
