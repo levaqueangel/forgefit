37308c761f761a71dacec67f2ce38ae839b8a8b3
levaqueangel <levaqueangel@gmail.com>
2026-06-03 14:05:40 +0200

    fix: correct firebase-admin import path in checkin-hebdo cron

--- forgefit/app/api/cron/checkin-hebdo/route.js
+++ forgefit/app/api/cron/checkin-hebdo/route.js
@@ -1,5 +1,5 @@
 import { Resend } from "resend";
-import { getAdminDb } from "../../firebase-admin";
+import { getAdminDb } from "../firebase-admin";
 export const dynamic = "force-dynamic";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
