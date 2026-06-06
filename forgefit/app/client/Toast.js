e4e00b42bf629bf4647e3bbd1a95942a5d21d5a6
Angel Levaque <angel@apxfitness.fr>
2026-06-06 19:15:59 +0200

    fix: export ToastContainer depuis Toast.js (crash espace client)

--- forgefit/app/client/Toast.js
+++ forgefit/app/client/Toast.js
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 
-function ToastContainer({ toasts }) {
+export function ToastContainer({ toasts }) {
   return (
     <div style={{
       position:"fixed",bottom:32,left:"50%",transform:"translateX(-50%)",
